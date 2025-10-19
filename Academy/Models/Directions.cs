fffc41389c1a4659c64accb6632f746ec22561b0
DmitryK777 <kislyakovdmv@gmail.com>
2025-10-19 13:05:00 +0300

    Academy

---
+++ Academy/Models/Directions.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Academy.Models
+{
+    public class Directions
+    {
+        [Key]
+        public byte direction_id {  get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
+    }
+}
